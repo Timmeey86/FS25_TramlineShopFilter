252455d092727f1ffef59b38711526030acfc451
Timmeey86 <Timmeey86@users.noreply.github.com>
2025-03-10 18:35:51 +0100

    exclude test folder from zip file

--- copytofs.bat
+++ copytofs.bat
@@ -2,4 +2,4 @@
 for %%a in ("%~dp0\.") do set "modname=%%~nxa"
 set "targetDir=%USERPROFILE%\Documents\my games\FarmingSimulator2025\mods"
 del -q "%targetDir%\%modname%.zip"
-robocopy . "%targetDir%\%modname%" /mir /XD ".git" ".vscode" "screenshots" /XF "*.bat" "*.md" "LICENSE" ".gitignore" ".gitattributes"
+robocopy . "%targetDir%\%modname%" /mir /XD ".git" ".vscode" "screenshots" "test" /XF "*.bat" "*.md" "LICENSE" ".gitignore" ".gitattributes"
