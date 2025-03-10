b49ad5559682cb493df9752d6d0c3595ba3a72c3
Timmeey86 <Timmeey86@users.noreply.github.com>
2025-03-10 21:03:04 +0100

    optimized code (tests still succeed)

--- scripts/TramlineWidthChecker.lua
+++ scripts/TramlineWidthChecker.lua
@@ -1,7 +1,8 @@
 ---This class is responsible for figuring out whether or not a certain work width is compatible with a tramline width
 ---@class TramlineWidthChecker
+---@field EPSILON number @The maximum floating point imprecision we tolerate
 TramlineWidthChecker = {
-	EPSILON = 0.001 -- The maximum floating point imprecision we tolerate
+	EPSILON = 0.001
 }
 
 ---Checks whether or not two numbers are almost equal, respecting floating point imprecision
@@ -12,30 +13,13 @@ local function nearlyEqual(first, second)
 	return math.abs(first - second) < TramlineWidthChecker.EPSILON
 end
 
----Checks whether or not a number is less than another number, respecting floating point imprecision
----@param first number @The first number
----@param second number @The second number
----@return boolean @True if first is less than second
-local function lessThan(first, second)
-	if nearlyEqual(first, second) then
-		return false -- they are either equal or there's only floating point imprecision
-	end
-	return first < second
-end
-
 ---Checks whether or not a number is between two other numbers, respecting floating point imprecision
 ---@param value number @The value
 ---@param lowerBound number @The lower bound
 ---@param upperBound number @The upper bound
 ---@return boolean @True if value is within [lowerBound, upperBound]
 local function between(value, lowerBound, upperBound)
-	if lessThan(lowerBound, value) and lessThan(value,upperBound) then
-		return true
-	end
-	if nearlyEqual(value, lowerBound) or nearlyEqual(value, upperBound) then
-		return true
-	end
-	return false
+	return value >= lowerBound - TramlineWidthChecker.EPSILON and value <= upperBound + TramlineWidthChecker.EPSILON
 end
 
 ---Calculates wether or not the given work width is compatible with the given tramline width
@@ -51,14 +35,12 @@ function TramlineWidthChecker.workWidthIsCompatible(workWidth, tramlineWidth, to
 	end
 
 	local width = workWidth
-	while lessThan(width, tramlineWidth - tolerance) do
+	while width <= tramlineWidth + tolerance do
+		if between(width, tramlineWidth - tolerance, tramlineWidth + tolerance) then
+			return true
+		end
 		width = width + workWidth
 	end
-	if between(width, tramlineWidth - tolerance, tramlineWidth + tolerance) then
-		return true
-	end
-	if nearlyEqual(width + workWidth, tramlineWidth) then
-		return true
-	end
+
 	return false
 end
